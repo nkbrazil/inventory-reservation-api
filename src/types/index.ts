703fd512e9ebfc313ed09e6a301fbddd5c1c012c
Niro brazil <nirobrazil31@yahoo.com>
2026-01-15 23:32:59 +0800

    initial commit

---
+++ src/types/index.ts
@@ -0,0 +1,37 @@
+// item type
+export type Item = {
+  id: string;
+  item_name: string;
+  total_quantiy: string;
+  created_at: string;
+  updated_at: string;
+};
+
+// reservation type
+export type Reservation = {
+  id: string;
+  item_id: string;
+  customer_id: string;
+  quantity: number;
+  status: ReservationStatuses;
+  expires_at: string;
+  created_at: string;
+  updated_at: string;
+};
+
+// statuses
+export type ReservationStatuses =
+  | "CONFIRMED"
+  | "CANCELLED"
+  | "PENDING"
+  | "EXPIRED";
+
+// helper type for monitoring of quantities
+export type ItemStatus = {
+  id: string;
+  item_name: string;
+  total_quantity: number;
+  available_quantity: number;
+  reserved_quantity: number;
+  confirmed_quantity: number;
+};
